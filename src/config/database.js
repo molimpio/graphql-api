5ff74679d77b9f5ce2501f34c858d796d2c87f10
Moises Olimpio <moiza.olimpio@gmail.com>
2018-10-18 21:15:21 -0300

    Parte 1

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+const knexfile = require('../knexfile')
+const knex = require('knex')(knexfile)
+module.exports = knex
+
+knex('users').insert({
+    name: 'João',
+    email: 'joao@gmail.com',
+    password: 'joao'
+}).then(data => console.log(data))
+
+knex('users').then(res => console.log(res))
